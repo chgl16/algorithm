6ca1fd47c2f367876260c10248f7153621d6ccd6
chgl16 <chenbetter1996@gmail.com>
2019-03-10 11:19:42 +0800

    修复快速排序下标越界bug

--- 排序/QuickSort.java
+++ 排序/QuickSort.java
@@ -1,51 +1,43 @@
-package xyz.cglzwz.question_bank.sort;
+package xyz.cglzwz.question_bank;
 
 public class QuickSort {
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] s = new int[] {0, 2, 3, 9, 1};
+		quickSort(s, 0, s.length-1);
 		
-		int[] n = new int[] {5, 2, 3, 9, 1};
-		new QuickSort().quickSort(n, 0, n.length-1);
-		for (int x : n) {
-			System.out.println(x + " ");
+		for (int x : s) {
+			System.out.println(x);
 		}
 	}
+
 	
-	
-	public void quickSort(int[] a, int left, int right) {
+	public static void quickSort(int[] a, int left, int right) {
+		// 必须先比较，不然分分钟下标越界
+		if (left >= right)
+			return;
+		
 		int i = left;
 		int j = right;
+		// 取第一个数做轴枢
 		int pivot = a[left];
 		
-		if (left >= right) {
-			return;
-		}
 		
-		// 右起扫描，过滤比枢轴大的
-		while (j > i && a[j] > pivot) 
-			j--;
-		if (j > i) {
-			// 直接复制右边第一个小于枢轴的元素给a[i]
+		while (i < j) {
+			// 从右往左扫描，过滤比轴枢大的
+			while (i < j && a[j] >= pivot) j--;   
+			//交换比枢轴小的记录到左端
 			a[i] = a[j];
-		}
-		
-		// 左起扫描，过滤比枢轴小的
-		while (j > i && a[i] < pivot) 
-			i++;
-		
-		if (j > i) {
-			// 交换了
+			// 从左往右扫描，过滤比轴枢小的
+			while (i < j && a[i] <= pivot) i++;
+			//交换比枢轴大的记录到右端
 			a[j] = a[i];
 		}
 		
-		// 基准值插入 i位置
+		// 插入轴枢，此时 i = j
 		a[i] = pivot;
 		
 		quickSort(a, left, i - 1);
 		quickSort(a, i + 1, right);
 	}
-	
-	
-
 }
